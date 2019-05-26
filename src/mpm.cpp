45cc7dd62812dde3839d91eff033cd35a54606da
Liby Lee <liby99@icloud.com>
2019-05-25 18:59:39 -0700

    Working simple file

--- src/mpm.cpp
+++ src/mpm.cpp
@@ -1,7 +1,11 @@
 #include "mpm.h"
 
+#include <Eigen/Dense>
+
 using namespace mpm;
 
-int some_func(int i) {
+int mpm::some_func(int i) {
+  Eigen::Matrix2f m;
+  m << 1.0, 2.0, 3.0, 4.0;
   return i + 1;
 }
--- tests/sphere.cpp
+++ tests/sphere.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "mpm.h"
 
 int main(int argc, char** argv) {
   std::cout << "Hello World\n" << std::endl;
+  std::cout << mpm::some_func(3) << std::endl;
 }
